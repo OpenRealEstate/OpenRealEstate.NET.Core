7eb5a6aa69e6412a849ee3435bcfe61fb0a6a5fd
Pure Krome <pure.krome@world-domination.com.au>
2020-05-18 20:28:59 +1000

    Side now inherits a UnitOfMeasure.

--- src/OpenRealEstate.Core/Side.cs
+++ src/OpenRealEstate.Core/Side.cs
@@ -1,12 +1,10 @@
 namespace OpenRealEstate.Core
 {
-    public class Side
+    public class Side : UnitOfMeasure
     {
         /// <summary>
         /// Which side is this? Left, Right, Front, etc?
         /// </summary>
         public string Name { get; set; }
-
-        public UnitOfMeasure Length { get; set; }
     }
 }
